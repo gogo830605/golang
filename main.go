1effdfc536b2dd14b98f721aa0de5de438a30e1f
Daniel <daniel.hu@in-win.com.tw>
2021-01-22 11:19:07 +0800

    golang practice

--- main.go
+++ main.go
@@ -1,10 +1,52 @@
 package main
 
 import (
-	"inwin/routes"
+	// "inwin/routes"
+	"fmt"
+	// "time"
 )
 
+func BuildMaxHeap(in []int) []int {
+	heap := []int{}
+	for _, value := range in {
+		// insert a new node at the bottom
+		heap = append(heap, value)
+		// do heapify to make the new node to right position
+		heap = MaxHeapifyBottomUp(heap)
+	}
+
+	return heap
+}
+
+func MaxHeapifyBottomUp(in []int) []int {
+	// the index of new node, the latest one element in slice
+	newELeIndex := len(in) - 1
+	for {
+		// get the parent node index
+		parentIndex := (newELeIndex+1)/2 - 1
+		// break if the node is to the top of heap
+		if parentIndex < 0 {
+			break
+		}
+		// if parent node < the new node, swap
+		if in[newELeIndex] > in[parentIndex] {
+			in[parentIndex], in[newELeIndex] = in[newELeIndex], in[parentIndex]
+			newELeIndex = parentIndex
+		} else {
+			// if not, break here
+			break
+		}
+	}
+	return in
+}
+
 func main() {
-	router := router.SetupRouter()
-	router.Run(":8888")
+	// router.SetupRouter()
+	// router := router.SetupRouter()
+	// router.Run(":8888")
+	// c1 := make(chan string)
+    // c2 := make(chan string)
+
+    arr := []int{1, 2, 3, 4, 5, 5, 6, 6, 1, 2, 10, 9, 20}
+    fmt.Println(BuildMaxHeap(arr))
 }
--- services/service.go
+++ services/service.go
@@ -1,7 +1,65 @@
 package service
 
 import (
-
+	"database/sql"
+	"fmt"
+  	"strings"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func 
+const (
+	userName = "root"
+	password = "honte511"
+	host     = "127.0.0.1"
+	port     = "3306"
+	dbName   = "inwin_estore3"
+)
+
+
+func InitDB() int {
+	// [username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
+	// "username:password@tcp(host:port)/數據庫?charset=utf8"
+	path := strings.Join([]string{userName, ":", password, "@tcp(", host, ":", port, ")/", dbName, "?charset=utf8"}, "")
+	// fmt.Println(path)
+
+	// 第一個是 driverName 第二個則是 database 的設定 path
+	// 也可以用 var DB *sql.DB
+	DB, _ := sql.Open("mysql", path)
+
+	// 設定 database 最大連接數
+	DB.SetConnMaxLifetime(100)
+
+	//設定上 database 最大閒置連接數
+	DB.SetMaxIdleConns(10)
+
+	// 驗證是否連上 db
+	if err := DB.Ping(); err != nil {
+		fmt.Println("opon database fail:", err)
+		// return
+	}
+	fmt.Println("connnect success")
+	var (
+        id       int
+        title     string
+        // quantity int
+    )
+	rows, err := DB.Query("SELECT id, title FROM banners where id = 1 order by sort desc")
+	checkError(err)
+	rows.Scan(&id, &title)
+	defer rows.Close()
+	return id;
+    // fmt.Println("Reading data:")
+    // for rows.Next() {
+    //     err := rows.Scan(&id, &title)
+    //     checkError(err)
+    //     fmt.Printf("Data row = (%d, %s)\n", id, title)
+	// }
+	// err = rows.Err()
+    // checkError(err)
+    // fmt.Println("Done.")
+}
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
